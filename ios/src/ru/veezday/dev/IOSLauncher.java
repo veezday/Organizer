a1e8952a81e6d43680da4cad7dddc6e9f802e1b3
veezday <vlad-zav07@mail.ru>
2023-10-07 16:24:06 +0700

    init

---
+++ ios/src/ru/veezday/dev/IOSLauncher.java
@@ -0,0 +1,22 @@
+package ru.veezday.dev;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import ru.veezday.dev.Organizer;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Organizer(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
